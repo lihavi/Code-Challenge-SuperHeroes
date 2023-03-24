1e5c159f58b2c8b8cbfd038746e5f0f5666dcaaf
vivianne <viviannjoroge91@gmail.com>
2023-03-24 17:42:10 +0300

    set up the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :powers, only: [:show, :index,:update]
+  resources :hero_powers, only: [:show, :index, :create, :update]
+  resources :heroes, only: [:index, :show]
+
 end
